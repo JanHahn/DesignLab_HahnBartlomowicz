989bc8a33e40bce8ab56e4a5f4c6c68a8b930530
janhahn <janekhahn1@gmail.com>
2024-11-05 04:30:08 +0100

    renamed branch

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import (QMainWindow, QApplication, QLabel, QPushButton, QSt
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 
-
+#hello
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
